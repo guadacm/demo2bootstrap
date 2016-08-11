3471d2541f0444d27273ff1e337a77dde5f4beec
Guada <guada.m@gmail.com>
2016-08-10 22:41:05 -0300

    actualizacion

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(function(){
+    
+    // -- Cargar menu
+    $.ajax("/header.html").done(function(result){
+        $("#menu").html(result);
+    });
+
+    // -- Cargar footer
+    $.ajax("/footer.html").done(function(result){
+        $("#footer").html(result);
+    });
+
+    // -- Cargar pagina 1
+    $.ajax("/pages/1.html").done(function(result){
+        $("#principal").html(result);
+    });
+
+    // -- Carga cuando se hace click en algun item del menu
+    $(document).on("click","#menu-principal li > a",function(e){
+        e.preventDefault();
+        var url = $(this).attr("href");
+
+        // -- Cargo el contenido de la pagina en el div principal
+        $.ajax(url).done(function(result){
+            $("#principal").html(result);
+        });
+    });
+
+    // $(document).on("click", "#buscar", function(e){
+    //     e.preventDefault();
+    //     var url = $(this).attr()
+    // });
+});
